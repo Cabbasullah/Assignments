4260ea6ffa5ad479bc193ef31aea94050578c26a
AbbasIman <85749516+Cabbasullah@users.noreply.github.com>
2022-09-26 20:47:39 +0700

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,33 @@
+#question 1 of the assignment
+print("Enter your height: ")
+f = int(input("f(int:)"))
+i = int(input("i(int:)"))
+h = f*12*2.54+i*2.54
+l = h*88/100
+print(l)
+
+
+
+
+
+
+
+#question 2 of the asisgnment
+f , m = eval(input("enter force, mass separated by comma: ," "kg , N:   "))
+a = f/m
+print(a)
+
+
+#question 3 of the assignment
+
+a =  "A Shinigami"
+c = "my"
+nn = "out of the "
+nb = "as if he were a vegetarian fishing a "
+nd = "out of his salad."
+b = (input("enter verb:  "))
+f = (input("enter adjective:  "))
+n = (input("enter noun:  "))
+na = (input("enter noun:  "))
+nc= (input("enter noun:  "))
+print (a+" "+b+" "+c+" "+f+" "+n+" "+nn+" "+na+" "+nb+" "+nc+" "+nd)
---
+++ practice.py
@@ -0,0 +1,14 @@
+
+
+
+a =  "A Shinigami"
+c = "my"
+nn = "out of the "
+nb = "as if he were a vegetarian fishing a "
+nd = "out of his salad."
+b = (input("enter verb:  "))
+f = (input("enter adjective:  "))
+n = (input("enter noun:  "))
+na = (input("enter noun:  "))
+nc= (input("enter noun:  "))
+print (a+" "+b+" "+c+" "+f+" "+n+" "+nn+" "+na+" "+nb+" "+nc+" "+nd)
